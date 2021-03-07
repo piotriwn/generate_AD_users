15ed4ec2a4729251a77b9b7e50f1cffe50727bdb
Piotr Iwaniec <piotrek.iwn@gmail.com>
2021-03-07 18:43:16 +0100

    Added owners and exported files to csv

--- generate_AD_users.py
+++ generate_AD_users.py
@@ -1,4 +1,7 @@
 import random
+import csv
+import re
+import copy
 
 OFFICE_DICT = {"England" : ("London", "Cambridge", "Bristol", "Liverpool"), "United States" : ("New York", "Dallas", "San Francisco", "Chicago"), "Canada": ("Ottawa", "Vancouver", "Quebec"), "India": ("Mombai", "Delhi", "Bangalore")}
 
@@ -23,9 +26,13 @@ class Employee:
         self.password = "Zaq12wsx"
         self.groups = []
         self.groupsOwner = []
+        self.propertyList = None
 
     def showProperties(self):
         return f"{self.name:<15} {self.surname:<20} {self.username:<10} {self.emailAlias:<40} {self.managerLevel:<3} {self.manager:<10} {self.office:<15} {self.OULevel1:<15} {self.OULevel2:<15} {self.password:<10} {self.groups}"
+
+    def writePropertiesToList(self):
+        self.propertyList = [self.name, self.surname, self.username, self.emailAlias, self.managerLevel, self.manager, self.office, self.OULevel1, self.OULevel2, self.password, self.groups, self.groupsOwner]
 
 
 def createUserObjects(k):
@@ -106,6 +113,7 @@ def createRandomGroups(k):
         groupsDict[item] = []
     return groupsDict
 
+
 def assignToGroups(employeeList, groupsDict):
     # employeeList has to be sorted by emp.managerLevel attribute
     employeeList.sort(key = lambda x: x.managerLevel)
@@ -121,12 +129,28 @@ def assignToGroups(employeeList, groupsDict):
                 emp.groups.append("Administrators")
                 groupsDict["Administrators"].append(emp.username)
                 if emp.managerLevel <= 2:
-                    emp.groups.append("Board")
+                    emp.groups.append("Board") 
                     groupsDict["Board"].append(emp.username)
         for group in groupsDict:
-            if (group not in ["Administrators", "Board", "VIP", "Office license"]) and (random.randint(0, numberOfGroups) == 0): # give everyone 1/numberOfGroups chance to be a member of every group
+            if (group not in ["Administrators", "Board", "VIP", "Office license"]) and (random.randint(0, numberOfGroups) in [0,1]): # give everyone 2/numberOfGroups chance to be a member of every group
                 emp.groups.append(group)
                 groupsDict[group].append(emp.username) 
+    return None
+
+
+def assignOwners(employeeList, groupsDict):
+    randomGroupRegex = re.compile(r'GroupNo\w+')
+    groupNameList = [x for x in groupsDict.keys()]
+    for group in groupNameList:
+        if randomGroupRegex.search(group):
+            random.choice(employeeList).groupsOwner.append(group)
+        elif group in  ["Administrators", "Board", "VIP"]:
+            random.choice([x for x in empList if x.managerLevel == 2]).groupsOwner.append(group)
+        elif group in ["IT Support", "Developers", "HR"]:
+            random.choice([x for x in empList if x.managerLevel in [2,3]]).groupsOwner.append(group)
+    return None
+            
+
 
 def printStatistics(employeeList):
     print(f"# CEO = {sum(1 for x in employeeList if x.managerLevel == 1)}\n# Level 2 = {sum(1 for x in empList if x.managerLevel == 2)}\n# Level 3 = {sum(1 for x in empList if x.managerLevel == 3)}\n# Level 4 = {sum(1 for x in empList if x.managerLevel == 4)}\n# Level 5 = {sum(1 for x in empList if x.managerLevel == 5)}\n# Level 6 = {sum(1 for x in empList if x.managerLevel == 6)}\n# Level 7 = {sum(1 for x in empList if x.managerLevel == 7)}")
@@ -136,25 +160,40 @@ def printStatistics(employeeList):
 
 with open("names2000.csv", 'r', encoding="utf-8-sig") as namesFile:
     with open("surnames1000.csv",'r', encoding="utf-8-sig") as surnamesFile:
-        namesList = namesFile.readlines()
-        surnamesList = surnamesFile.readlines()
-        namesList = [name.strip() for name in namesList]
-        surnamesList = [surnname.strip() for surnname in surnamesList]
-        
-        k = input("Provide the number of AD user objects to be created: ")
-        
-        empList = createUserObjects(k)
-        checkUsernameUniq(empList)
-        modifyEmailAlias(empList)
-        defineManagers(empList)
-        assignToOffice(empList)
-        groupsDict = createRandomGroups(k)
-        assignToGroups(empList, groupsDict)
+        with open("employees.csv", mode='w', newline='') as employeeData:
+            with open("groups.csv", mode="w", newline='') as groupData:
+                employeeWriter = csv.writer(employeeData, delimiter=";", quotechar='"', quoting=csv.QUOTE_ALL)
+                groupWriter = csv.writer(groupData, delimiter=";", quotechar='"', quoting=csv.QUOTE_ALL)
 
-        for i in range(100):
-            print(sorted(empList,key = lambda x: x.managerLevel)[i].showProperties())
+                namesList = namesFile.readlines()
+                surnamesList = surnamesFile.readlines()
+                namesList = [name.strip() for name in namesList]
+                surnamesList = [surnname.strip() for surnname in surnamesList]
+                
+                k = input("Provide the number of AD user objects to be created: ")
+                
+                empList = createUserObjects(k)
+                checkUsernameUniq(empList)
+                modifyEmailAlias(empList)
+                defineManagers(empList)
+                assignToOffice(empList)
+                groupsDict = createRandomGroups(k)
+                assignToGroups(empList, groupsDict)
+                assignOwners(empList, groupsDict)
 
-        printStatistics(empList)
+                for emp in empList:
+                    emp.writePropertiesToList()
+                    employeeWriter.writerow(emp.propertyList)
+
+                groupWriter.writerows(groupsDict.items())
+
+                # print(empList[10])
+                # print(groupsDict["GroupNo10"])
+
+                # for i in range(100):
+                    # print(sorted(empList,key = lambda x: x.managerLevel)[i].showProperties())
+
+                # printStatistics(empList)
 
         
 
